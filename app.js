8e2ab009dbb96435479da330b9196e378115412b
jaesu968 <mazterk968@gmail.com>
2026-05-27 11:12:53 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+// import body-parser to parse request bodies
+const bodyParser = require('body-parser');
+// import cors to handle CORS requests
+const cors = require('cors');
+
+module.exports = app;
+
+// Add middleware for handling CORS requests from index.html
+app.use(cors());
+
+
+// Add middware for parsing request bodies here:
+app.use(bodyParser.json());
+
+
+// Mount your existing apiRouter below at the '/api' path.
+const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
+
+
